4465f2cd19ce49468a9d2fa7988ee76aa993c43b
PngWnA <pngwna@pngwna.net>
2025-10-13 16:04:16 +0900

    Extend 1 node to worker plane

--- blueprint/nodes.tf
+++ blueprint/nodes.tf
@@ -3,7 +3,8 @@ locals {
     "Sun" = { role = "Controller", ip = "10.24.68.1/24", gw = "10.24.0.1", vm_id = 1000000}
     "Mercury" = { role = "Worker", ip = "10.24.68.2/24", gw = "10.24.0.1", vm_id = 1000001 }
     "Venus" = { role = "Worker", ip = "10.24.68.3/24", gw = "10.24.0.1", vm_id = 1000002 }
-    #"Earth" = { role = "Worker", ip = "10.24.68.4/24", gw = "10.24.0.1", vm_id = 1000003 }
+    "Earth" = { role = "Worker", ip = "10.24.68.4/24", gw = "10.24.0.1", vm_id = 1000003 }
+    # Expand nodes if needed
     #"Mars" = { role = "Worker", ip = "10.24.68.5/24", gw = "10.24.0.1", vm_id = 1000004 }
     #"Jupiter" = { role = "Worker", ip = "10.24.68.6/24", gw = "10.24.0.1", vm_id = 1000005 }
     #"Saturn" = { role = "Worker", ip = "10.24.68.7/24", gw = "10.24.0.1", vm_id = 1000006 }
